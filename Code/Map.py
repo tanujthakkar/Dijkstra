ef287b8326ccd718d4728beb8b171fa1d71007a9
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-03-05 14:58:09 -0500

    Circle class complete

--- Code/Map.py
+++ Code/Map.py
@@ -20,42 +20,48 @@ import cv2
 
 class Map():
 
-	def __init__(self, height, width):
+	def __init__(self, height: int, width: int) -> None:
 		self.height = height
 		self.width = width
-		xx, yy = np.mgrid[0:height,0:width]
-		self.occupancy_grid = np.zeros([self.height+4, self.width+4], dtype=bool)
+		self.xx, self.yy = np.mgrid[0:height+4,0:width+4]
+		self.occupancy_grid = np.zeros([self.height+4, self.width+4], dtype=int)
 
-	def generate_map(self):
-		self.occupancy_grid[:2] = True
-		self.occupancy_grid[:,:2] = True
-		self.occupancy_grid[-2:] = True
-		self.occupancy_grid[:,-2:] = True
-		plt.plot(np.where(self.occupancy_grid==True)[0], np.where(self.occupancy_grid==True)[1], 'ks')
+	def generate_map(self) -> None:
+		c = Circle(300, 185, 40)
+		circle = c.generate_circle(self.xx, self.yy)
+		self.occupancy_grid += circle
+
+		c = Circle(300, 185, 45)
+		clearance = c.generate_circle(self.xx, self.yy)
+		self.occupancy_grid += clearance
+
+		self.occupancy_grid[:2] = -1
+		self.occupancy_grid[:,:2] = -1
+		self.occupancy_grid[-2:] = -1
+		self.occupancy_grid[:,-2:] = -1
+
+		plt.plot(np.where(self.occupancy_grid==-1)[0], np.where(self.occupancy_grid==-1)[1], 'ks')
+		plt.plot(np.where(self.occupancy_grid==1)[0], np.where(self.occupancy_grid==1)[1], '.')
+		plt.plot(np.where(self.occupancy_grid>1)[0], np.where(self.occupancy_grid>1)[1], '.k')
 		plt.show()
 
 
 class Circle():
 
-	def __init__(self, x, y, radius):
+	def __init__(self, x: int, y: int, radius: int) -> None:
 		self.name = "Circle"
 		self.x = x
 		self.y = y
 		self.radius = radius
-		self.occupancy_grid = np.zeros([self.radius, self.radius], dtype=bool)
 
-	def generate_circle(self):
-		xx, yy = np.mgrid[0:self.radius*2,0:self.radius*2]
-		self.occupancy_grid = np.logical_and(True, (((xx - self.x) ** 2 + (yy - self.y) ** 2) <= self.radius**2))
-		# print(self.occupancy_grid.shape)
-		# plt.plot(np.where(self.occupancy_grid==True)[0], np.where(self.occupancy_grid==True)[1], 'ks')
-		# plt.show()
+	def generate_circle(self, xx, yy) -> np.array:
+		occupancy_grid = np.logical_and(True, (((xx - self.x) ** 2 + (yy - self.y) ** 2) <= self.radius**2))
+		occupancy_grid[occupancy_grid == True] = 1
+
+		return occupancy_grid
 
 def main():
 	m = Map(400,250)
-	c = Circle(40, 40, 40)
-	c.generate_circle()
-	m.occupancy_grid[120:200,120:200] = c.occupancy_grid
 	m.generate_map()
 
 if __name__ == '__main__':
